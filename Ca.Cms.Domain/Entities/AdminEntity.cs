75489bc3bcd5b0f2cb018a5c5ac1f46678de36a7
alcb7 <canbulan1849@gmail.com>
2024-02-08 15:36:52 +0300

    entity

---
+++ Ca.Cms.Domain/Entities/AdminEntity.cs
@@ -0,0 +1,30 @@
+﻿using Ca.Cms.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ca.Cms.Domain.Entities
+{
+    public class AdminEntity : BaseEntity
+    {
+        
+        public string Name { get; set; } 
+
+        public string Surname { get; set; } 
+
+        public string Email { get; set; } 
+
+        public string Password { get; set; } 
+
+        public string Phone { get; set; } 
+
+
+        public string Address { get; set; } 
+       
+        public string? Cv { get; set; } 
+    }
+}
