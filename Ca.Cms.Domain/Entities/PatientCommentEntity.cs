a5e6f766f09485a32bb51152893bac5e96adbc70
mehmet134 <mehmetkrs42@gmail.com>
2024-02-08 15:34:55 +0300

    entites

---
+++ Ca.Cms.Domain/Entities/PatientCommentEntity.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ca.Cms.Domain.Common;
+
+namespace Ca.Cms.Domain.Entities
+{
+    public class PatientCommentEntity : BaseEntity
+    {
+        public int PatientId { get; set; }
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public PatientEntity? Patient { get; set; }
+    }
+}
---
+++ Ca.Cms.Domain/Entities/ServiceBlogEntity.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ca.Cms.Domain.Common;
+
+namespace Ca.Cms.Domain.Entities
+{
+    public class ServiceBlogEntity : BaseEntity
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string? ResimDosyaAdi { get; set; }
+
+        public string? ResimYolu
+        {
+            get
+            {
+                if (!string.IsNullOrEmpty(ResimDosyaAdi))
+                {
+                    return "/images/" + ResimDosyaAdi; // wwwroot klasöründeki images altındaki dosyaya göre yol belirtilir.
+                }
+                return null;
+            }
+        }
+    }
+}
