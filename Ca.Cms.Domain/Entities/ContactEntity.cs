e2820e14033e1cf75b4cbd5c37d5ab0449db8dbf
alcb7 <canbulan1849@gmail.com>
2024-02-08 15:50:14 +0300

    Revert "Revert "entity""
    This reverts commit b2a4df4913d33f02bfaa465eedcf34ddeca9016f.

---
+++ Ca.Cms.Domain/Entities/ContactEntity.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ca.Cms.Domain.Common;
+
+namespace Ca.Cms.Domain.Entities
+{
+    public class ContactEntity : BaseEntity
+    {
+     
+        public string Fullname { get; set; } 
+        public string Topic { get; set; }
+
+        public string Email { get; set; }
+
+        public string Phone { get; set; }
+
+        public string Text { get; set; }
+    }
+}
