413504273ad762df3e5e7a1cf4461dcfb60d3563
alcb7 <canbulan1849@gmail.com>
2024-03-21 06:45:08 +0300

    .

---
+++ Ca.Cms.Infrastructure/Authentication/ApplicationUser.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+
+
+namespace Ca.Cms.Infrastructure.Authentication
+{
+    //public class ApplicationUser : IdentityUser
+    //{
+    //    public string? FirstName { get; set; }
+    //    public string? LastName { get; set; }
+    //    public string? ActivationCode { get; set; }
+    //    public string? RefreshToken { get; set; }
+    //}
+}
